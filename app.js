e7bf93e2cef95c10262296ade056a4c609709666
EMRE YAZICI <emre@EMREs-MacBook-Pro.local>
2021-10-27 15:34:09 +0300

    commit 02

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 const express = require("express");
+const ejs = require("ejs")
 
 const app = express();
+
+// Template Engine
+app.set("view engine","ejs")
+
+// Middlewares
+app.use(express.static("public"))
+
+// Routes
+app.get("/",(req,res) => {
+  res.status(200).render("index", {page_name: "index"})
+})
+
+app.get("/about",(req,res) => {
+  res.status(200).render("about", {page_name: "about"})
+})
+
+app.get("/courses",(req,res) => {
+  res.status(200).render("courses", {page_name: "courses"})
+})
+
+app.get("/contact",(req,res) => {
+  res.status(200).render("contact", {page_name: "contact"})
+})
+
+app.get("/dashboard",(req,res) => {
+  res.status(200).render("dashboard", {page_name: "dashboard"})
+})
+
 
 const port = 3000;
 app.listen(port, () => {
